1ec6bebd353a6d542a46afe3dcdd2a4626ca37db
foxandfeature <foxandfeature@gmail.com>
2026-06-01 22:44:07 +0200

    -

--- process.lua
+++ process.lua
@@ -1,24 +1,42 @@
--- Initializes the Lua generation environment.
-function init_function()
+-- Global variables required by tilemaker to determine which keys to index
+node_keys = {
+    "landuse",
+    "building",
+    "bridge",
+    "foot",
+    "highway",
+    "tourism",
+    "amenity",
+    "shelter_type"
+}
+
+way_keys = {
+    "landuse",
+    "building",
+    "bridge",
+    "foot",
+    "highway",
+    "tourism",
+    "amenity",
+    "shelter_type"
+}
+
+function init_function(name, is_first)
 end
 
--- Clean up function executed after tile processing completes.
 function exit_function()
 end
 
--- Evaluates OSM tags to determine the station name
--- and its chronological itinerary order.
-local function determineStationName(obj)
-    local landuse = obj:Find("landuse") or ""
-    local building = obj:Find("building") or ""
-    local bridge = obj:Find("bridge") or ""
-    local foot = obj:Find("foot") or ""
-    local highway = obj:Find("highway") or ""
-    local tourism = obj:Find("tourism") or ""
-    local amenity = obj:Find("amenity") or ""
-    local shelter_type = obj:Find("shelter_type") or ""
+local function determineStationName()
+    local landuse = Find("landuse")
+    local building = Find("building")
+    local bridge = Find("bridge")
+    local foot = Find("foot")
+    local highway = Find("highway")
+    local tourism = Find("tourism")
+    local amenity = Find("amenity")
+    local shelter_type = Find("shelter_type")
 
-    -- 1. Maggot Hof (First stop of the journey)
     if landuse == "farmyard" then
         return "Maggot Hof", 1
     end
@@ -31,7 +49,6 @@ local function determineStationName(obj)
         return "Maggot Hof", 1
     end
 
-    -- 2. Brandywine Ferry (Second stop of the journey)
     if bridge ~= "" and bridge ~= "no" then
         if foot == "yes"
             or foot == "designated"
@@ -47,7 +64,6 @@ local function determineStationName(obj)
         end
     end
 
-    -- 3. Tom Bombadil House (Third stop of the journey)
     if tourism == "wilderness_hut"
         or tourism == "alpine_hut"
         or tourism == "cabin"
@@ -59,27 +75,23 @@ local function determineStationName(obj)
         return "Tom Bombadil House", 3
     end
 
-    return "Unknown", -1
+    return nil, nil
 end
 
--- Processing logic applied to every OSM node.
-function node_function(node)
-    local name, order = determineStationName(node)
-
-    if name ~= "Unknown" then
-        node:Layer("journey", false)
-        node:Attribute("station_name", name)
-        node:AttributeNumeric("order", order)
+function node_function()
+    local name, order = determineStationName()
+    if name then
+        Layer("journey", false)
+        Attribute("station_name", name)
+        AttributeInteger("order", order)
     end
 end
 
--- Processing logic applied to every OSM way.
-function way_function(way)
-    local name, order = determineStationName(way)
-
-    if name ~= "Unknown" then
-        way:Layer("journey", way:IsClosed())
-        way:Attribute("station_name", name)
-        way:AttributeNumeric("order", order)
+function way_function()
+    local name, order = determineStationName()
+    if name then
+        Layer("journey", IsClosed())
+        Attribute("station_name", name)
+        AttributeInteger("order", order)
     end
 end
