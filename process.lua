ffc4a5045c0fbbb7b9f258e1abf372b5e023dadf
foxandfeature <foxandfeature@gmail.com>
2026-06-02 19:08:44 +0200

    -

--- process.lua
+++ process.lua
@@ -1,102 +1,62 @@
-node_keys = {
-    "landuse",
-    "building",
-    "bridge",
-    "foot",
-    "highway",
-    "tourism",
-    "amenity",
-    "shelter_type"
-}
+-- Key definitions for Tilemaker indexing
+node_keys = { "landuse", "building", "bridge", "foot", "highway", "tourism", "amenity", "shelter_type" }
+way_keys =  { "landuse", "building", "bridge", "foot", "highway", "tourism", "amenity", "shelter_type" }
 
-way_keys = {
-    "landuse",
-    "building",
-    "bridge",
-    "foot",
-    "highway",
-    "tourism",
-    "amenity",
-    "shelter_type"
-}
+-- Match tables for fast lookups (optimizes performance by replacing long 'or' chains)
+local farm_buildings = { farm = true, barn = true, stable = true, sty = true, cowshed = true }
+local ferry_feet = { yes = true, designated = true, use_sidepath = true }
+local ferry_highways = { foot = true, track = true, path = true, pedestrian = true }
+local huts = { wilderness_hut = true, alpine_hut = true, cabin = true, hut = true }
 
-function init_function()
-end
+function init_function() end
+function exit_function() end
 
-function exit_function()
-end
+-- Central logic to determine station order based on OSM tags
+local function determineStationOrder(obj)
+    -- Maggot Hof
+    local landuse = obj:Find("landuse")
+    if landuse == "farmyard" then return "1" end
 
-local function determineStationName(obj)
-    local landuse = obj:Find("landuse") or ""
-    local building = obj:Find("building") or ""
-    local bridge = obj:Find("bridge") or ""
-    local foot = obj:Find("foot") or ""
-    local highway = obj:Find("highway") or ""
-    local tourism = obj:Find("tourism") or ""
-    local amenity = obj:Find("amenity") or ""
-    local shelter_type = obj:Find("shelter_type") or ""
+    local building = obj:Find("building")
+    if farm_buildings[building] then return "1" end
 
-    if landuse == "farmyard" then
-        return "Maggot Hof", "1"
-    end
-
-    if building == "farm"
-        or building == "barn"
-        or building == "stable"
-        or building == "sty"
-        or building == "cowshed" then
-        return "Maggot Hof", "1"
-    end
-
-    if bridge ~= "" and bridge ~= "no" then
-        if foot == "yes"
-            or foot == "designated"
-            or foot == "use_sidepath" then
-            return "Brandywine Ferry", "2"
-        end
-
-        if highway == "foot"
-            or highway == "track"
-            or highway == "path"
-            or highway == "pedestrian" then
-            return "Brandywine Ferry", "2"
+    -- Brandywine Ferry
+    local bridge = obj:Find("bridge")
+    if bridge and bridge ~= "" and bridge ~= "no" then
+        if ferry_feet[obj:Find("foot")] or ferry_highways[obj:Find("highway")] then
+            return "2"
         end
     end
 
-    if tourism == "wilderness_hut"
-        or tourism == "alpine_hut"
-        or tourism == "cabin"
-        or tourism == "hut" then
-        return "Tom Bombadil House", "3"
+    -- Tom Bombadil House"
+    if huts[obj:Find("tourism")] then return "3" end
+
+    if obj:Find("amenity") == "shelter" and obj:Find("shelter_type") ~= "public_transport" then
+        return "3"
     end
 
-    if amenity == "shelter"
-        and shelter_type ~= "public_transport" then
-        return "Tom Bombadil House", "3"
-    end
-
-    return nil, nil
+    return nil
 end
 
+-- Process node elements (points)
 function node_function(node)
-    local name, order = determineStationName(node)
-
-    if name ~= nil then
+    local order = determineStationOrder(node)
+    if order then
         node:Layer("journey", false)
-        node:Attribute("station_name", name)
         node:Attribute("order", order)
     end
 end
 
+-- Process way elements (lines/polygons)
 function way_function(way)
-    local name, order = determineStationName(way)
-
-    if name ~= nil then
+    local order = determineStationOrder(way)
+    if order then
+        -- Tilemaker built-in: Converts the way geometry into a single point at its center (centroid)
+        way:Centroid()
+        
         way:Layer("journey", false)
-        way:Attribute("station_name", name)
         way:Attribute("order", order)
     end
 end
 
-function relation_function(relation)
-end
+function relation_function(relation) end
