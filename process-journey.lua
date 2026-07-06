5965c50ca83596ffab228e18fb1d78a957ef2723
foxandfeature <foxandfeature@gmail.com>
2026-07-06 20:55:50 +0200

    :)

--- process-journey.lua
+++ process-journey.lua
@@ -1,7 +1,7 @@
 -- Shared indexing keys for Tilemaker
-local common_keys = { 
+local common_keys = {
     "building", "bridge", "foot", "highway", "tourism", "amenity", "shelter_type",
-    "archaeological_site", "historic", "cemetery", "man_made", "tomb", "landuse", 
+    "archaeological_site", "historic", "cemetery", "man_made", "tomb", "landuse",
     "memorial", "ruins", "tower:type", "ford", "natural", "tunnel", "waterway",
     "leisure", "water", "barrier", "place_of_worship"
 }
@@ -11,30 +11,30 @@ way_keys = common_keys
 -- ==========================================================
 -- Lookup Sets (O(1) optimization)
 -- ==========================================================
-local farm_buildings      = { farm = true, barn = true, stable = true, sty = true, cowshed = true }
-local huts                = { wilderness_hut = true, cabin = true, hut = true }
-local arch_sites          = { tumulus = true, megalith = true, necropolis = true, tomb = true, earthwork = true, grave_field = true }
-local historic_tombs      = { tomb = true, cemetery = true }
-local man_made_tombs      = { grave = true, tomb = true }
-local memorials           = { war_memorial = true, cross = true, cenotaph = true }
-local bree_amenities      = { pub = true, bar = true, biergarten = true, restaurant = true }
-local bree_tourism        = { hotel = true, hostel = true, motel = true, alpine_hut = true }
+local farm_buildings = { farm = true, barn = true, stable = true, sty = true, cowshed = true }
+local huts = { wilderness_hut = true, cabin = true, hut = true }
+local arch_sites = { tumulus = true, megalith = true, necropolis = true, tomb = true, earthwork = true, grave_field = true }
+local historic_tombs = { tomb = true, cemetery = true }
+local man_made_tombs = { grave = true, tomb = true }
+local memorials = { war_memorial = true, cross = true, cenotaph = true }
+local bree_amenities = { pub = true, bar = true, biergarten = true, restaurant = true }
+local bree_tourism = { hotel = true, hostel = true, motel = true, alpine_hut = true }
 
 -- General pedestrian lookups
-local pedestrian_status   = { yes = true, designated = true, use_sidepath = true }
+local pedestrian_status = { yes = true, designated = true, use_sidepath = true }
 local pedestrian_highways = { foot = true, track = true, path = true, pedestrian = true, residential = true }
 
 -- Station Lookups
-local amon_sul_ruins         = { castle = true, tower = true }
-local rivendell_buildings    = { church = true, cathedral = true, mosque = true, synagogue = true, temple = true, monastery = true }
-local rivendell_amenities    = { place_of_worship = true, monastery = true }
-local moria_man_made         = { mineshaft = true, adit = true, cellar_entrance = true }
-local moria_historic         = { mine_shaft = true }
-local moria_natural          = { cave_entrance = true }
-local moria_tunnels          = { yes = true }
-local morannon_barriers      = { gate = true, wicket_gate = true, entrance = true, kissing_gate = true, sally_port = true }
+local amon_sul_ruins = { castle = true, tower = true }
+local rivendell_buildings = { church = true, cathedral = true, mosque = true, synagogue = true, temple = true, monastery = true }
+local rivendell_amenities = { place_of_worship = true, monastery = true }
+local moria_man_made = { mineshaft = true, adit = true, cellar_entrance = true }
+local moria_historic = { mine_shaft = true }
+local moria_natural = { cave_entrance = true }
+local moria_tunnels = { yes = true }
+local morannon_barriers = { gate = true, wicket_gate = true, entrance = true, kissing_gate = true, sally_port = true }
 local minas_tirith_buildings = { townhall = true, government = true, manor = true, palace = true }
-local havens_man_made        = { pier = true, quay = true }
+local havens_man_made = { pier = true, quay = true }
 
 -- ==========================================================
 -- Core Functions
@@ -45,31 +45,31 @@ function exit_function() end
 -- Central logic to determine station order based on OSM tags
 local function determineStationOrder()
     -- Fetch all tags once per feature
-    local building     = Find("building")
-    local bridge       = Find("bridge")
-    local tourism      = Find("tourism")
-    local amenity      = Find("amenity")
-    local arch_site    = Find("archaeological_site")
-    local historic     = Find("historic")
-    local man_made     = Find("man_made")
-    local memorial     = Find("memorial")
-    local landuse      = Find("landuse")
-    local cemetery     = Find("cemetery")
-    local tomb         = Find("tomb")
-    local ruins        = Find("ruins")
-    local ford         = Find("ford")
-    local natural      = Find("natural")
-    local tunnel       = Find("tunnel")
+    local building = Find("building")
+    local bridge = Find("bridge")
+    local tourism = Find("tourism")
+    local amenity = Find("amenity")
+    local arch_site = Find("archaeological_site")
+    local historic = Find("historic")
+    local man_made = Find("man_made")
+    local memorial = Find("memorial")
+    local landuse = Find("landuse")
+    local cemetery = Find("cemetery")
+    local tomb = Find("tomb")
+    local ruins = Find("ruins")
+    local ford = Find("ford")
+    local natural = Find("natural")
+    local tunnel = Find("tunnel")
     local shelter_type = Find("shelter_type")
-    local highway      = Find("highway")
-    local foot         = Find("foot")
-    local waterway     = Find("waterway")
-    local water        = Find("water")
-    local barrier      = Find("barrier")
+    local highway = Find("highway")
+    local foot = Find("foot")
+    local waterway = Find("waterway")
+    local water = Find("water")
+    local barrier = Find("barrier")
 
     -- 1. Maggot Hof
-    if farm_buildings[building] then 
-        return "1" 
+    if farm_buildings[building] then
+        return "1"
     end
 
     -- 2. Brandywine Ferry (Placeholder for real ferries later)
@@ -78,30 +78,30 @@ local function determineStationOrder()
     end
 
     -- 3. Tom Bombadil House
-    if huts[tourism] or (amenity == "shelter" and shelter_type ~= "public_transport" and shelter_type ~= "rock_shelter") then 
-        return "3" 
+    if huts[tourism] or (amenity == "shelter" and shelter_type ~= "public_transport" and shelter_type ~= "rock_shelter") then
+        return "3"
     end
 
     -- 4. Barrow-downs
-    if arch_sites[arch_site] or historic_tombs[historic] or man_made_tombs[man_made] or memorials[memorial] or 
-       landuse == "cemetery" or amenity == "grave_yard" or cemetery ~= "" or tomb ~= "" then
+    if arch_sites[arch_site] or historic_tombs[historic] or man_made_tombs[man_made] or memorials[memorial]
+            or landuse == "cemetery" or amenity == "grave_yard" or cemetery ~= "" or tomb ~= "" then
         return "4"
     end
 
     -- 5. Bree (The Prancing Pony Inn)
-    if bree_amenities[amenity] or bree_tourism[tourism] then 
-        return "5" 
+    if bree_amenities[amenity] or bree_tourism[tourism] then
+        return "5"
     end
 
     -- 6. Amon Sûl
-    if arch_site == "fortification" or amon_sul_ruins[ruins] or (historic == "castle" and ruins == "yes") or 
-       tourism == "viewpoint" or Find("tower:type") == "observation" then 
-        return "6" 
+    if arch_site == "fortification" or amon_sul_ruins[ruins] or (historic == "castle" and ruins == "yes")
+            or tourism == "viewpoint" or Find("tower:type") == "observation" then
+        return "6"
     end
 
     -- 7. Ford of Bruinen
-    if ford == "yes" or ford == "stepping_stones" then 
-        return "7" 
+    if ford == "yes" or ford == "stepping_stones" then
+        return "7"
     end
 
     -- 8. Rivendell
@@ -110,8 +110,8 @@ local function determineStationOrder()
     end
 
     -- 9. Mines of Moria
-    if moria_man_made[man_made] or moria_historic[historic] or moria_natural[natural] or shelter_type == "rock_shelter" or 
-       (moria_tunnels[tunnel] and (pedestrian_status[foot] or pedestrian_highways[highway])) then
+    if moria_man_made[man_made] or moria_historic[historic] or moria_natural[natural] or shelter_type == "rock_shelter"
+            or (moria_tunnels[tunnel] and (pedestrian_status[foot] or pedestrian_highways[highway])) then
         return "9"
     end
 
